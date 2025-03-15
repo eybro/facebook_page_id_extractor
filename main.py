26a2ef72c6245bb00b1297eebaaf92113b9203c8
eybro <erik.j.raberg@gmail.com>
2025-03-15 16:25:18 +0100

    added headers

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ with open('pages.txt', 'r') as f:
 page_id_dict = {}
 
 for page in page_list:
-    request = requests.get(f"https://facebook.com/{page}")
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+        }
+    request = requests.get(f"https://facebook.com/{page}", headers=headers)
     time.sleep(5)
     soup = BeautifulSoup(request.content, "html.parser")
     head_content = soup.head
 
     meta_tag = soup.find("meta", {"property": "al:android:url"})
-
     if meta_tag:
         android_url = meta_tag.get("content")
+        print(f"{page} : {android_url}")
         page_id_dict[page] = android_url.split("/")[-1]
 
 with open("output.txt", "w") as f:
